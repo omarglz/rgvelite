bdb7b35231e1cea5e6e071f9a37470b0edb62ce2
Omar Gonzalez <omar596@mit.edu>
2016-12-28 13:16:22 -0600

    better initial setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,27 @@
-var webpack = require('webpack');
-var path = require('path');
+var path = require("path");
 
-var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
-var APP_DIR = path.resolve(__dirname, 'src/client/app');
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
 
 var config = {
-  entry: APP_DIR + '/index.jsx',
-  output: {
-    path: BUILD_DIR,
-    filename: 'bundle.js'
-  },
-  module : {
-  	loaders : [
-  		{
-	  		test : /\.jsx?/,
-	        include : APP_DIR,
-	        loader: 'babel'
-  		}
-  	]
-  }
+    entry: SRC_DIR + "/app/index.js",
+    output: {
+        path: DIST_DIR + "/app",
+        filename: "bundle.js",
+        publicPath: "/app/"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js?/,
+                include: SRC_DIR,
+                loader: "babel-loader",
+                query: {
+                    presets: ["react", "es2015", "stage-2"]
+                }
+            }
+        ]
+    }
 };
 
 module.exports = config;
